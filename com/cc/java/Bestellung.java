4dfaecb776df6b1dfa42881568ebe2cf430f7820
J.T.J <jonasthomasjanssen@gmail.com>
2026-02-26 11:16:52 +0000

    04 Literation Noch ein Getraenk ?

--- com/cc/java/Bestellung.java
+++ com/cc/java/Bestellung.java
@@ -10,13 +10,47 @@ public static void main(String[] args) {
         String drink = "noch kein Getränk gewählt";
         String itemNumStr;
         String flag;
- 
+
         System.out.println(food);
         System.out.println(drink);
 
-    
 
-    System.out.println(" Test Bestellung ");
+ // Bestelldialog 1 : Ausgabe
+        System.out.println("-------------------------------");
+        System.out.println("Herzlich Willkommen bei MD, Ihre Bestellung bitte!");
+        System.out.println("Hamburger(1), Cheeseburger(2) oder Chilliburger(3) ?");
+
+        // Bestelldialog 1 : Eingabe
+        itemNumStr = System.console().readLine("Ihre Wahl? : ");
+        System.out.println(itemNumStr);  // 1 oder 2 oder 3
+
+
+         // Zuordnung Zahl --> Burger
+        switch (itemNumStr)
+        {
+            case "1":
+                food = "Hamburger";
+                break;
+            case "2":
+                food = "Cheeseburger";
+                break;
+            case "3":
+                food = "Chilliburger";
+                break;
+        }
+
+     // Entscheidung Getränk dazu?
+        System.out.println("-------------------------------");
+        System.out.println("Möchten Sie ein Getränk dazu bestellen? (j/n)");
+       flag = System.console().readLine("(j/n)? :");
+
+
+
+        
+
+
+
+
 }
 
 }
