e7b354f85aab44b49af4fe39519676b3a37d1a1e
J.T.J <jonasthomasjanssen@gmail.com>
2026-02-26 10:46:16 +0000

    01 Iteration: Variablen

---
+++ com/cc/java/Bestellung.java
@@ -0,0 +1,23 @@
+package com.cc.java;
+
+public class Bestellung {
+
+
+public static void main(String[] args) {
+
+// Variablen deklarieren
+        String food = "noch kein Burger gewählt";
+        String drink = "noch kein Getränk gewählt";
+        String itemNumStr;
+        String flag;
+ 
+        System.out.println(food);
+        System.out.println(drink);
+
+    
+
+    System.out.println(" Test Bestellung ");
+}
+
+}
+
